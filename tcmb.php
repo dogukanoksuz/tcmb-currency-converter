9222e929e93ef22161be7dddde5dd6e49bf496d3
Doğukan Öksüz <root@dogukanoksuz.com.tr>
2019-04-01 22:48:45 +0300

    fix indentation, add comment blocks

--- tcmb.php
+++ tcmb.php
@@ -4,30 +4,40 @@
 	 * github.com/dogukanoksuz
 	 */
 	function TCMB_Converter ( $currency_from, $currency_to ) {
+		// check if there is simplexml extension
 		if (!function_exists("simplexml_load_file")) return;
-
-        if ($currency_from == "TRY") $currency_from_val = 1;
-        if ($currency_to   == "TRY") $currency_to_val   = 1;
-
+		
+		// tcmb database is calculating all values by TRY reference
+        	if ($currency_from == "TRY") $currency_from_val = 1;
+        	if ($currency_to   == "TRY") $currency_to_val   = 1;
+		
+		// load file; http://www.tcmb.gov.tr/kurlar/today.xml
 		$tcmb = simplexml_load_file( 'http://www.tcmb.gov.tr/kurlar/today.xml' );
+		
+		// by xml structure the today.xml file
+		// $tcmb->Currency means each currency so we need to iterate all elements of Currency.
 		foreach ( $tcmb->Currency as $currency ) {
+			// this codeblock may be unneccessary and can be written short as
+			// $currency_from_val = (!currency->BanknoteSelling) ? (float) $currency->BanknoteSelling : (float) $currency->ForexSelling;
+			// i added there if checks because some currencies hasn't BanknoteSelling value.
 			if ($currency_from == $currency["CurrencyCode"]) {
-                if ($currency->BanknoteSelling != 0) {
-				    $currency_from_val = $currency->BanknoteSelling;
-                } else {
-                    $currency_from_val = $currency->ForexSelling;
-                }
+                		if ($currency->BanknoteSelling != 0) {
+				    $currency_from_val = (float) $currency->BanknoteSelling;
+                		} else {
+                		    $currency_from_val = (float) $currency->ForexSelling;
+                		}
 			}
 
 			if ($currency_to == $currency["CurrencyCode"]) {
 				if ($currency->BanknoteSelling != 0) {
-				    $currency_to_val = $currency->BanknoteSelling;
-                } else {
-                    $currency_to_val = $currency->ForexSelling;
-                }
+				    $currency_to_val = (float) $currency->BanknoteSelling;
+                		} else {
+                		    $currency_to_val = (float) $currency->ForexSelling;
+                		}
 			}
 		};
-
+		
+		// return value.
 		if ($currency_from || $currency_to != 0) {
 			return (float) round($currency_to_val / $currency_from_val, 10);
 		} else {
